d4c4ceef0d5e302eab0fca5feb07e734403af793
Jeswin Kumar <jeswinpk@agilehead.com>
2017-02-13 22:55:07 +0530

    Tests don't need async.

--- src/test/test.js
+++ src/test/test.js
@@ -13,32 +13,32 @@ function toArray(seq) {
   return results;
 }
 
-describe("lazily", async () => {
-  it(`should return a sequence`, async () => {
+describe("lazily", () => {
+  it(`should return a sequence`, () => {
     const seq = Seq.of([1, 2, 3])
     const results = toArray(seq);
     results.should.deepEqual([1, 2, 3])
   })
 
-  it(`should map() results`, async () => {
+  it(`should map() results`, () => {
     const seq = Seq.of([1, 2, 3]).map(x => x * 2)
     const results = toArray(seq);
     results.should.deepEqual([2, 4, 6])
   })
 
-  it(`should filter() results`, async () => {
+  it(`should filter() results`, () => {
     const seq = Seq.of([1, 2, 3, 4, 5]).filter(x => x > 2)
     const results = toArray(seq);
     results.should.deepEqual([3, 4, 5])
   })
 
-  it(`should exit() early`, async () => {
+  it(`should exit() early`, () => {
     const seq = Seq.of([1, 2, 3, 4, 5]).exit(x => x > 3)
     const results = toArray(seq);
     results.should.deepEqual([1, 2, 3])
   })
 
-  it(`exit() should not interrupt valid results`, async () => {
+  it(`exit() should not interrupt valid results`, () => {
     const seq = Seq.of([1, 2, 3, 4, 5])
       .map(x => x * 2)
       .exit(x => x > 4)
@@ -47,81 +47,81 @@ describe("lazily", async () => {
       results.should.deepEqual([20, 40])
   })
 
-  it(`find()`, async () => {
+  it(`find()`, () => {
     const result = Seq.of([1, 2, 3, 4, 5]).find(x => x * 10 === 30)
 
     result.should.equal(3);
   })
 
-  it(`reduce()`, async () => {
+  it(`reduce()`, () => {
     const result = Seq.of([1, 2, 3, 4, 5]).reduce((acc, x) => acc + x, 0)
     result.should.equal(15);
   })
 
-  it(`first()`, async () => {
+  it(`first()`, () => {
     const result = Seq.of([1, 2, 3, 4, 5]).first();
     result.should.equal(1);
   })
 
-  it(`last()`, async () => {
+  it(`last()`, () => {
     const result = Seq.of([1, 2, 3, 4, 5]).last();
     result.should.equal(5);
   })
 
-  it(`every()`, async () => {
+  it(`every()`, () => {
     const result = Seq.of([1, 2, 3, 4, 5]).every(x => x <= 5);
     result.should.be.ok();
   })
 
-  it(`every() negative`, async () => {
+  it(`every() negative`, () => {
     const result = Seq.of([1, 2, 3, 4, 5]).every(x => x < 5);
     result.should.not.be.ok();
   })
 
-  it(`some()`, async () => {
+  it(`some()`, () => {
     const result = Seq.of([1, 2, 3, 4, 5]).some(x => x === 3);
     result.should.be.ok();
   })
 
-  it(`some() negative`, async () => {
+  it(`some() negative`, () => {
     const result = Seq.of([1, 2, 3, 4, 5]).every(x => x === 10);
     result.should.not.be.ok();
   })
 
-  it(`toArray()`, async () => {
+  it(`toArray()`, () => {
     const result = Seq.of([1, 2, 3, 4, 5]).toArray();
     result.should.deepEqual([1, 2, 3, 4, 5])
   })
 
-  it(`includes()`, async () => {
+  it(`includes()`, () => {
     const result = Seq.of([1, 2, 3, 4, 5]).includes(3);
     result.should.be.ok();
   })
 
-  it(`includes() negative`, async () => {
+  it(`includes() negative`, () => {
     const result = Seq.of([1, 2, 3, 4, 5]).includes(10);
     result.should.not.be.ok();
   })
 
-  it(`concat()`, async () => {
+  it(`concat()`, () => {
     const seq = Seq.of([1, 2, 3, 4, 5]).concat(Seq.of([6, 7, 8]));
     const results = toArray(seq);
     results.should.deepEqual([1, 2, 3, 4, 5, 6, 7, 8]);
   })
 
-  it(`reverse()`, async () => {
+  it(`reverse()`, () => {
     const seq = Seq.of([1, 2, 3]).reverse();
     const results = toArray(seq);
     results.should.deepEqual([3, 2, 1])
   })
 
-  it(`slice(begin)`, async () => {
+  it(`slice(begin)`, () => {
     const seq = Seq.of([1, 2, 3, 4, 5]).slice(1);
     const results = toArray(seq);
     results.should.deepEqual([2, 3, 4, 5])
   })
 
-  it(`slice(begin, end)`, async () => {
+  it(`slice(begin, end)`, () => {
     const seq = Seq.of([1, 2, 3, 4, 5]).slice(1, 4);
     const results = toArray(seq);
     results.should.deepEqual([2, 3, 4])
